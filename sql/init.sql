6dbede1da040d36796ffba57a6b6f7c9e4692cd5
xdlkc <wimness@163.com>
2019-09-21 13:15:18 +0800

    s

--- sql/init.sql
+++ sql/init.sql
@@ -1,30 +1,54 @@
 create database zhihu;
 use zhihu;
-create table topic
-(
-  id           bigint auto_increment primary key,
-  topic_id     bigint       not null unique comment '话题id',
-  name         varchar(128) not null comment '话题名称',
-  url          varchar(128) not null comment '话题地址',
-  excerpt      text         not null comment '摘要',
-  introduction text         not null comment '概述',
-  avatar_url   varchar(255) not null comment '封面地址',
-  type         varchar(64),
-  category     varchar(64),
-  is_black     boolean           default false,
-  is_vote      boolean           default false,
-  create_time  timestamp    null default current_timestamp,
-  update_time  timestamp    null default current_timestamp on update current_timestamp,
+create table topic (
+  id bigint auto_increment primary key,
+  topic_id bigint not null unique comment '话题id',
+  name varchar(128) not null comment '话题名称',
+  url varchar(128) not null comment '话题地址',
+  excerpt text not null comment '摘要',
+  introduction text not null comment '概述',
+  avatar_url varchar(255) not null comment '封面地址',
+  type varchar(64),
+  category varchar(64),
+  is_black boolean default false,
+  is_vote boolean default false,
+  create_time timestamp null default current_timestamp,
+  update_time timestamp null default current_timestamp on update current_timestamp,
   unique index (topic_id)
 ) engine = innodb comment '话题表';
-
-create table topic_related
-(
-  id          bigint auto_increment primary key,
-  topic_id    bigint    not null comment '当前话题id',
-  children_id bigint    not null comment '子话题id',
+create table topic_related (
+  id bigint auto_increment primary key,
+  topic_id bigint not null comment '当前话题id',
+  children_id bigint not null comment '子话题id',
   create_time timestamp null default current_timestamp,
   update_time timestamp null default current_timestamp on update current_timestamp,
   foreign key (topic_id) references topic (id),
   foreign key (children_id) references topic (id)
 ) engine = innodb comment '父子话题关联表';
+create table zhihu.question(
+  id bigint auto_increment primary key,
+  question_id bigint not null,
+  question_created bigint not null,
+  url varchar(256) not null,
+  title text not null,
+  question_type varchar(64) not null,
+  type varchar(64) not null,
+  create_time timestamp null default current_timestamp,
+  update_time timestamp null default current_timestamp on update current_timestamp,
+) engine = innodb comment '问题表';
+create table zhihu.answer(
+  id bigint auto_increment primary key,
+  answer_id bigint not null,
+  voteup_count int,
+  excerpt text not null,
+  content text not null,
+  comment_count int not null,
+  create_time timestamp null default current_timestamp,
+  update_time timestamp null default current_timestamp on update current_timestamp,
+) engine = innodb comment '回答表';
+create table zhihu.article(
+  id bigint auto_increment primary key,
+  
+  create_time timestamp null default current_timestamp,
+  update_time timestamp null default current_timestamp on update current_timestamp,
+)
--- zhihu_spider/settings.py
+++ zhihu_spider/settings.py
@@ -119,7 +119,7 @@ USER_AGENT_LIST = [
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)',
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36',
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36']
-today = datetime.now()
-log_file_path = "log/{}-{}-{}.log".format(today.year, today.month, today.day)
-LOG_LEVEL = 'INFO'
-LOG_FILE = log_file_path
+# today = datetime.now()
+# log_file_path = "log/{}-{}-{}.log".format(today.year, today.month, today.day)
+# LOG_LEVEL = 'INFO'
+# LOG_FILE = log_file_path
